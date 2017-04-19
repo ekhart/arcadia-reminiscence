fee23bd0590a976749921cf3350e38006bb8f87a
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-19 18:15:04 +0200

    rotate card if rotate? state true

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -1,5 +1,27 @@
 (ns game.core
   (use arcadia.core))
 
-(log "Hello, from game.core")
+; (log "Hello, from game.core")
 
+(def card (object-named "Card"))
+
+(set-state! card :rotate? false)
+; (state card)
+; (update-state! card :rotate? (constantly true))
+
+(defn rotate-card [go]
+  (if (state card :rotate?)
+    (.. go transform (Rotate 0 -1 0))))
+
+(defn set-rotate?-card [go]
+  (update-state! card 
+                 :rotate? 
+                 #(not %)))
+
+(hook+ card 
+       :update 
+       #'game.core/rotate-card)
+
+(hook+ card 
+       :on-mouse-down 
+       #'game.core/set-rotate?-card)
