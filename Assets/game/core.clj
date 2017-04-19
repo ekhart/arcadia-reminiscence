a93ee4aff8486f2795394dc9fc53d080fe435a2d
Artur Dębkowski <arturdebkowski@gmail.com>
2017-04-19 18:28:53 +0200

    refact

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -4,24 +4,27 @@
 ; (log "Hello, from game.core")
 
 (def card (object-named "Card"))
+(def cards (objects-tagged "Card"))
 
-(set-state! card :rotate? false)
+; (set-state! card :rotate? false)
 ; (state card)
 ; (update-state! card :rotate? (constantly true))
 
 (defn rotate-card [go]
-  (if (state card :rotate?)
+  (if (state go :rotate?)
     (.. go transform (Rotate 0 -1 0))))
 
 (defn set-rotate?-card [go]
-  (update-state! card 
+  (update-state! go
                  :rotate? 
                  #(not %)))
 
-(hook+ card 
-       :update 
-       #'game.core/rotate-card)
+(doseq [card cards] 
+  (hook+ card
+         :update 
+         #'game.core/rotate-card))
 
-(hook+ card 
-       :on-mouse-down 
-       #'game.core/set-rotate?-card)
+(doseq [card cards]
+  (hook+ card
+         :on-mouse-down 
+         #'game.core/set-rotate?-card))
